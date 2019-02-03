7dbbd5d4a82986b0d1cae5a76af9af00fc733102
Robin Tom <robin.tom.73@gmail.com>
2019-02-03 16:55:38 +0530

    Environment variables created

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: API_KEY,
-  authDomain: DOMAIN_FIREBASE_URL,
-  databaseURL: Database_URL
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "catch-of-the-day-demo-2.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-demo-2.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
